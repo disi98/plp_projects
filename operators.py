7b5b15478ca896df22d1b73a5b350d839e33574e
Albert Disi Lungonzo <67419107+disi98@users.noreply.github.com>
2025-03-26 23:50:15 +0000

    Basic Calculator Program

---
+++ operators.py
@@ -0,0 +1,17 @@
+def opr(num1, num2, operator):
+    if operator == '+':
+        print(f"{num1} + {num2} = {num1 + num2}")
+    elif operator == '-':
+        print(f"{num1} + {num2} = {num1 - num2}")
+    elif operator == '*':
+        print(f"{num1} + {num2} = {num1 * num2}")
+    elif operator == '/':
+        print(f"{num1} + {num2} = {num1 / num2}")
+    else:
+        print('Invalid operator')
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+operator = input("Enter operator: ")
+opr(num1, num2, operator)
+
