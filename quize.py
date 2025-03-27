83e794471ec81f714afa2f5a83ab22db65e29ea7
Albert Disi Lungonzo <67419107+disi98@users.noreply.github.com>
2025-03-27 00:11:35 +0000

    week_2

---
+++ quize.py
@@ -0,0 +1,14 @@
+
+# 1. Write a program that accepts user input to create a list of integers. Then, compute the sum of all the integers in the list.
+# 2. Create a tuple containing the names of five of your favorite books. Then, use a for loop to print each book name on a separate line.
+# 3. Write a program that uses a dictionary to store information about a person, such as their name, age, and favorite color. Ask the user for input and store the information in the dictionary. Then, print the dictionary to the console.
+# 4. Write a program that accepts user input to create two sets of integers. Then, create a new set that contains only the elements that are common to both sets.
+# 5. Create a program that stores a list of words. Then, use list comprehension to create a new list that contains only the words that have an odd number of characters.
+
+# Create an empty list called my_list.
+# Append the following elements to my_list: 10, 20, 30, 40.
+# Insert the value 15 at the second position in the list.
+# Extend my_list with another list: [50, 60, 70].
+# Remove the last element from my_list.
+# Sort my_list in ascending order.
+# Find and print the index of the value 30 in my_list.
