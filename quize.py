4569d69e0fba4354fdd8d55076e755f6151e0b8c
Albert Disi Lungonzo <67419107+disi98@users.noreply.github.com>
2025-03-27 00:27:48 +0000

    week 3 assignment

--- quize.py
+++ quize.py
@@ -1,3 +1,4 @@
+# WEEK ONE QUIZ
 
 # 1. Write a program that accepts user input to create a list of integers. Then, compute the sum of all the integers in the list.
 # 2. Create a tuple containing the names of five of your favorite books. Then, use a for loop to print each book name on a separate line.
@@ -5,6 +6,8 @@
 # 4. Write a program that accepts user input to create two sets of integers. Then, create a new set that contains only the elements that are common to both sets.
 # 5. Create a program that stores a list of words. Then, use list comprehension to create a new list that contains only the words that have an odd number of characters.
 
+
+# WEEK TWO QUIZ
 # Create an empty list called my_list.
 # Append the following elements to my_list: 10, 20, 30, 40.
 # Insert the value 15 at the second position in the list.
@@ -12,3 +15,12 @@
 # Remove the last element from my_list.
 # Sort my_list in ascending order.
 # Find and print the index of the value 30 in my_list.
+
+
+# WEEK THREE QUIZ
+# Create a function named calculate_discount(price, discount_percent) that calculates the final price after applying a discount. The function should take the original price (price) and the discount percentage (discount_percent) as parameters. If the discount is 20% or higher, apply the discount; otherwise, return the original price.
+# Using the calculate_discount function, prompt the user to enter the original price of an item and the discount percentage. Print the final price after applying the discount, or if no discount was applied, print the original price.
+
+
+
+# WEEK FOUR QUIZ
---
+++ week_3_python_module_assignment.py
@@ -0,0 +1,14 @@
+# Create a function named calculate_discount(price, discount_percent) that calculates the final price after applying a discount. The function should take the original price (price) and the discount percentage (discount_percent) as parameters. If the discount is 20% or higher, apply the discount; otherwise, return the original price.
+# Using the calculate_discount function, prompt the user to enter the original price of an item and the discount percentage. Print the final price after applying the discount, or if no discount was applied, print the original price.
+
+
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        return price - (price * discount_percent / 100)
+    else:
+        return price
+    
+price = float(input("Original Price: "))
+discount_percent = float(input("Discount: "))
+final_price = calculate_discount(price, discount_percent)
+print(f"Final Price: {final_price}")
